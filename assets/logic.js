a1fa82158beeeecea62e9a9d5a7df45a8e982f05
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-09 16:01:49 -0500

    Adjust the README and add global styles

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,11 @@
+// shows only the upcoming dates grying out the past dates
+// new Calendar('.calendar', {
+//   style: 'background',
+//   minDate: new Date()
+// })
+
+
 new Calendar(
   document.querySelector('.calendar')
   );
+
