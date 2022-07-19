5e0f8e9e114cf0832af4316171d5de3c154b4f1b
Shaviya <shaviyavictor@gmail.com>
2022-07-19 06:51:34 -0500

    Update logic.js
    16th consecutive day

--- assets/logic.js
+++ assets/logic.js
@@ -1,9 +1,10 @@
-//  15th consecutive day of visiting StackOverFlow 
+//  16th consecutive day of visiting StackOverFlow 
 //  Goal 1: Hit the 30th mark for the Silver Badge
 //  Goal 2: Hit the 100th mark for the Gold Badge
 //  Goal 3: Update this recording and always create a PR to get to a streak of @least 90 days
 //  1,543 contributions in 17th of July 2022
 //  Aim to hit 2,022 contributions by 25th of December 2022
+//  19th of July I hit a new longest streak for GitHub contributions of 10
 
 // shows only the upcoming dates greying out the past dates
 new Calendar('.calendar', {
